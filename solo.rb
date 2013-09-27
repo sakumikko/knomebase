40a93891baf1d4df5403d5e0c650535db0886b33
Saku Laitinen <saku.laitinen@siili.fi>
2013-09-27 10:57:31 +0300

    try to fix paths

--- solo.rb
+++ solo.rb
@@ -1,6 +1,6 @@
 file_cache_path           "/tmp/chef-solo"
-data_bag_path             "/tmp/chef-solo/data_bags"
-encrypted_data_bag_secret "/tmp/chef-solo/data_bag_key"
-cookbook_path             [ "/tmp/chef-solo/site-cookbooks",
-                            "/tmp/chef-solo/cookbooks" ]
-role_path                 "/tmp/chef-solo/roles"
+data_bag_path             "/var/chef_run/knomebase/data_bags"
+encrypted_data_bag_secret "/var/chef_run/knomebase/data_bag_key"
+cookbook_path             [ "/var/chef_run/knomebase/site-cookbooks",
+                            "/var/chef_run/knomebase/cookbooks" ]
+role_path                 "/var/chef_run/knomebase/roles"
